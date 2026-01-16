111d1534597e8b519abd666063d9eaa0e4410245
Gudi Indhu Reddy <153417426+gudireddy-0110@users.noreply.github.com>
2026-01-16 17:43:34 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+from routes.upload_routes import upload_bp
+from routes.timetable_routes import timetable_bp
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(upload_bp)
+app.register_blueprint(timetable_bp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
